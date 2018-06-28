ba25d2b614d70359f267d717fc85a23c0062a726
csmidt95 <34967998+csmidt95@users.noreply.github.com>
2018-06-28 12:51:14 -0500

    Update price_range.py

--- price_range.py
+++ price_range.py
@@ -1,3 +1,7 @@
+#The purpose of this exercise is to demonstrate how I solved a problem
+#After graphing industries vs stock price for companies in the S&P 500, I wanted to subcategorize the industries by price ranges
+#In this excersize, I split up the data into groups, I add information to identify the data groups, and then I recombine the groups.
+
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -9,24 +13,25 @@ Sectors = ["Information Technology","Utilities", "Industrials", "Energy" , "Real
   "Consumer Staples", "Consumer Discretionary"]
 
 
-
-
+#Here's how I seperated the groups. I had to use .loc to permenatly make changes to the dataframe; without it, pandas would 
+#make a copy and the program wouldn't function.
 less_than_50 = df.loc[df['Price'] < 50]
 between_50_and_100 = df.loc[(df['Price'] >=50) & (df['Price'] < 100)]
 between_100_and_500 = df.loc[(df.Price >= 100) & (df.Price < 500)]
 greater_than_500 = df.loc[df.Price > 500]
 
-
+#Here's where I added a characteristic to help me distinguish these groups. I knew I wanted to use the "hue" function in seaborn, 
+#so this seemed like the best way to let me use that function appropriately.
 less_than_50.loc[:,"Price_Range"] = "Less than 50" 
 between_50_and_100.loc[:,"Price_Range"] = "Between 50 and 100"
 between_100_and_500.loc[:,"Price_Range"]  = "Between 100 and 500"
 greater_than_500.loc[:,"Price_Range"]  = "Greater than 500"
 
-
-
+#Here's where I recombine the dataframes.
 frames = [less_than_50, between_50_and_100, between_100_and_500, greater_than_500]
 result = pd.concat(frames)
 
+#Here's where I plot my figure and adjust the styling 
 fig = plt.figure(figsize = (30,20))
 ax = sns.barplot(data = result, x = "Sector", y = "Price", estimator = len, hue = "Price_Range")
 plt.ylabel("Number of Companies", fontsize = 35)
